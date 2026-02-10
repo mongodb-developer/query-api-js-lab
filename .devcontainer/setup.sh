09a435d1da5e982c514e25bab114fe9776f92fd7
Diego Freniche <diego@freniche.com>
2026-02-10 10:18:26 +0100

    Reindenting lines

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,29 +2,29 @@
 
 echo ✅ APT GET UPDATE
 echo ✅ --------------
- apt-get update -y
+apt-get update -y
 
 #  apt-get install python3.8
 
 echo ✅ Install Jupyter notebook
 echo ✅ ------------------------
 # Install Jupyter notebook
- apt-get install jupyter -y
- apt-get clean packages
+apt-get install jupyter -y
+apt-get clean packages
 
 echo ✅ Install Jupyter Python kernel
 echo ✅ -----------------------------
 # Python Kernel
- apt-get install python3-pymongo -y
- apt-get clean packages
+apt-get install python3-pymongo -y
+apt-get clean packages
 
 echo ✅ Install Deno
 echo ✅ ------------
 # Install deno - Nodejs modern engine
- apt-get install npm -y
- npm install -g deno
+apt-get install npm -y
+npm install -g deno
 export PATH="$PATH:/home/vscode/lib/node_modules/deno"
- apt-get clean packages
+apt-get clean packages
 
 echo ✅ Install Deno Jupyter Kernel
 echo ✅ ---------------------------
@@ -33,7 +33,7 @@ deno jupyter --install
 echo ✅ Install MongoDB Deno Driver
 echo ✅ ---------------------------
 # Install MongoDB Deno driver
- deno install --global --allow-net --allow-read https://deno.land/x/mongo@v0.31.1/mod.ts
+deno install --global --allow-net --allow-read https://deno.land/x/mongo@v0.31.1/mod.ts
 
 echo ✅ Install cURL
 echo ✅ ----------------
@@ -43,6 +43,6 @@ apt-get clean packages
 echo ✅ Install Node.js v23 
 echo ✅ ----------------
 # Install Node.js 23 as MongoDB MCP server needs at least v20 (https://github.com/mongodb-js/mongodb-mcp-server?tab=readme-ov-file#prerequisites)
- npm install -g n
- n 23
+npm install -g n
+n 23
 hash -r 
